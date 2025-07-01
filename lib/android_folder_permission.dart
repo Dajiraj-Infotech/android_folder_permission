eaaf3550bccc4f86d36bff117564d406c9e68583
kumarpalsinh25 <info@dajiraj.com>
2025-07-01 18:52:55 +0530

    dart format

--- lib/android_folder_permission.dart
+++ lib/android_folder_permission.dart
@@ -1,9 +1,10 @@
-
 import 'android_folder_permission_platform_interface.dart';
 
 class AndroidFolderPermission {
   Future<bool> checkFolderPermission({required String path}) {
-    return AndroidFolderPermissionPlatform.instance.checkFolderPermission(folderPath: path);
+    return AndroidFolderPermissionPlatform.instance.checkFolderPermission(
+      folderPath: path,
+    );
   }
 
   Future<String> requestFolderPermission({required String path}) {
